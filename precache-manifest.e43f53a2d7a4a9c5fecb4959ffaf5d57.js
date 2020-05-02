44acd5783ebe63405bbd638ec9472a1687a54684
jilesanmi <ilesanmi.josiah@gmail.com>
2020-05-02 19:40:51 +0100

    Updates

---
+++ precache-manifest.e43f53a2d7a4a9c5fecb4959ffaf5d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027e0d8f4958f6b1fb87d684eca1c409",
+    "url": "/stargazin/index.html"
+  },
+  {
+    "revision": "92d146aef3ffb7e225b1",
+    "url": "/stargazin/static/css/main.fe7c82f3.chunk.css"
+  },
+  {
+    "revision": "2c5dc0fec90ad9d8af67",
+    "url": "/stargazin/static/js/2.56d344b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stargazin/static/js/2.56d344b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d146aef3ffb7e225b1",
+    "url": "/stargazin/static/js/main.7c3ebb90.chunk.js"
+  },
+  {
+    "revision": "282157f233696802e38e",
+    "url": "/stargazin/static/js/runtime-main.571750ad.js"
+  }
+]);
